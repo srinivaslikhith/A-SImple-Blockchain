b566c2a345f98f4a003b8b58a36fda32c4a9b9e4
srinivaslikhith <likhithsrinivas9@gmail.com>
2025-03-11 18:49:14 -0400

    add utils

--- headers/utils.h
+++ headers/utils.h
@@ -8,13 +8,4 @@ int64_t getCurrentTimestamp() {
     ).count();
 }
 
-std::string sha256(const std::string &input) {
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
-
-    std::stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-    }
-    return ss.str();
-}
+std::string sha256(const std::string &input);
